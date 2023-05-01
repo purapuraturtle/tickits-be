36f017925494e30be60f302afb6f1e101efac240
Abdul Rosid <rosid.dev@yahoo.com>
2023-05-01 20:15:14 +0700

    add code to edit data and get detail

--- src/routers/auth.route.js
+++ src/routers/auth.route.js
@@ -2,10 +2,22 @@ const { Router } = require("express");
 const authRouter = Router();
 
 const authController = require("../controllers/auth.controller");
+const authentication = require("../middlewares/auth");
+const upload = require("../middlewares/memoryUpload");
 
 authRouter.post("/register", authController.register);
 authRouter.post("/login", authController.login);
 authRouter.post("/forgot-password", authController.forgotPassword);
 authRouter.post("/reset-password/:id", authController.resetPassword);
-
+authRouter.get(
+  "/detail",
+  authentication.checkToken,
+  authController.getDataProfile
+);
+authRouter.patch(
+  "/edit",
+  authentication.checkToken,
+  upload.single("image"),
+  authController.editDataUsers
+);
 module.exports = authRouter;
