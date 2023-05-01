f0a9eefb2554d308018acd7487d9b91d93cb6e7b
afif-buchori <4fifbuchori@gmail.com>
2023-05-01 17:34:09 +0800

    create schedule multi data

--- src/models/teather.models.js
+++ src/models/teather.models.js
@@ -2,16 +2,36 @@ const db = require("../config/supabase");
 
 const createSchedule = async (body) => {
   return new Promise((resolve, reject) => {
-    const { teather_id, open_date, open_time, price, movie_id } = body;
-    const sqlQuery =
-      "INSERT INTO teather_studio (teather_id, open_date, open_time, price, movie_id) VALUES ($1, $2, $3, $4, $5) RETURNING *";
-    const values = [teather_id, open_date, open_time, price, movie_id];
+    let sqlQuery =
+      "INSERT INTO teather_studio (teather_id, open_date, open_time, price, movie_id) VALUES ";
+    let values = [];
+    body.forEach((element, idx) => {
+      if (values.length) sqlQuery += ", ";
+      const { teather_id, open_date, open_time, price, movie_id } = element;
+      sqlQuery += `($${1 + 5 * idx}, $${2 + 5 * idx}, $${3 + 5 * idx}, $${
+        4 + 5 * idx
+      }, $${5 + 5 * idx})`;
+      values.push(teather_id, open_date, open_time, price, movie_id);
+    });
     db.query(sqlQuery, values, (err, result) => {
       if (err) return reject(err);
       resolve(result);
     });
   });
 };
+
+// const createSchedule = async (body) => {
+//   return new Promise((resolve, reject) => {
+//     const { teather_id, open_date, open_time, price, movie_id } = body;
+//     const sqlQuery =
+//       "INSERT INTO teather_studio (teather_id, open_date, open_time, price, movie_id) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+//     const values = [teather_id, open_date, open_time, price, movie_id];
+//     db.query(sqlQuery, values, (err, result) => {
+//       if (err) return reject(err);
+//       resolve(result);
+//     });
+//   });
+// };
 
 module.exports = {
   createSchedule,
