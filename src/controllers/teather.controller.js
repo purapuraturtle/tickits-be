e4afbbc7a7e920d9a72f32bcdaf17e36405e10d1
Abdul Rosid <rosid.dev@yahoo.com>
2023-05-03 19:26:12 +0700

    fix code to search in open_date

--- src/controllers/teather.controller.js
+++ src/controllers/teather.controller.js
@@ -19,7 +19,6 @@ const createSchedule = async (req, res) => {
 const readDataStudio = async (req, res) => {
   try {
     let { open_date } = req.query;
-    open_date = open_date || new Date().toISOString().split("T")[0];
     const data = await teatherModels.getDataStudio();
     const result = data.reduce((acc, cur) => {
       const existingItem = acc.find(
@@ -49,6 +48,7 @@ const readDataStudio = async (req, res) => {
 
       return acc;
     }, []);
+
     console.log(open_date);
     const newResult = open_date
       ? result.filter((item) => {
